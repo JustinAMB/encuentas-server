c3b4048c318cc6895e970a147848e4e5c0463689
JustinAMB <Justinmb@outlook.es>
2022-08-04 00:22:14 -0600

    Se crea el servicio de auth

---
+++ Service/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using encuestas.Models.Request;
+using encuestas.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace encuestas.Service
+{
+    public interface IUserService
+    {
+        UserResponse Auth(AuthRequest model);
+        UserResponse ValidateToken(int id);
+    }
+}
---
+++ Service/UserService.cs
@@ -0,0 +1,29 @@
+﻿using encuestas.Common;
+using encuestas.Models.Request;
+using encuestas.Models.Response;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace encuestas.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly AppSettings _appSettings;
+        public UserService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+        public UserResponse Auth(AuthRequest model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserResponse ValidateToken(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
