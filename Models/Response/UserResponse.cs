44775640c324f1eff78d90479653aec0ab8c9936
JustinAMB <Justinmb@outlook.es>
2022-08-03 23:44:32 -0600

    Se crean las clases para respondera las peticones Auth

---
+++ Models/Response/UserResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace encuestas.Models.Response
+{
+    public class UserResponse
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Token { get; set; }
+    }
+}
