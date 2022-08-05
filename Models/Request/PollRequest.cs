c3cd984016621c143a2f7127fdc48e4217b295a4
JustinAMB <Justinmb@outlook.es>
2022-08-05 00:18:33 -0600

    se crean las clases para enviar y recibir los modelos

---
+++ Models/Request/PollRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace encuestas.Models.Request
+{
+    public class PollRequest
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Options { get; set; }
+        [Required]
+        public string Finish { get; set; }
+        [Required]
+        public int Author { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ Models/Response/PollResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace encuestas.Models.Response
+{
+    public class PollResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Options {get; set;}
+        public string Token { get; set; }
+        public string Finish { get; set; }
+        public UserResponse Author { get; set; }
+
+    }
+}
