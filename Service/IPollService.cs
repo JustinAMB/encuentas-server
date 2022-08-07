0edda4b1c73cb032093a82208f5f5f45a28f8d88
JustinAMB <Justinmb@outlook.es>
2022-08-06 23:52:58 -0600

    se crea el servicio para poll

---
+++ Service/IPollService.cs
@@ -0,0 +1,16 @@
+﻿using encuestas.Models.Request;
+using encuestas.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace encuestas.Service
+{
+    public interface IPollService
+    {
+        PollResponse Create(PollRequest poll);
+        List<PollResponse> SearchByAuthor(int author);
+        PollResponse SearchById(int poll);
+    }
+}
---
+++ Service/PollService.cs
@@ -0,0 +1,34 @@
+﻿using encuestas.Models.Dal;
+using encuestas.Models.Request;
+using encuestas.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace encuestas.Service
+{
+    public class PollService : IPollService
+    {
+        private PollDal dal;
+        public PollService() {
+            this.dal = new PollDal();
+        }
+
+        public PollResponse Create(PollRequest poll)
+        {
+            return dal.insertPoll(poll);
+           
+        }
+
+        public List<PollResponse> SearchByAuthor(int author)
+        {
+            return dal.getPollsByAuthor(author);
+        }
+
+        public PollResponse SearchById(int poll)
+        {
+            return dal.getPoll(poll);
+        }
+    }
+}
